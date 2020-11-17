15ce0802e441e253c0b879a03d0e95935b5d1ea8
JoseLuisGarGar <joseluisgarciagarrachon@gmail.com>
2020-11-17 19:25:03 +0100

    tarea

---
+++ practicas/p-iaw-4/ejercicio2-5.php
@@ -0,0 +1,23 @@
+<html>
+    <head>
+        <title>Ejercicio2-5</title>
+        <style>
+            body {
+                line-height:25px;
+            }
+        </style>
+    </head>
+    <body>    
+        <?php
+         $n1=245;
+         $n2=24;
+         if ($n1 > $n2) {
+            echo "$n1 es mayor a $n2";
+         }elseif ($n2 > $n2) {
+            echo "$n1 es mayor a $n2";
+         } else {
+            echo "Son iguales";
+         }
+        ?>
+    </body>
+</html>
