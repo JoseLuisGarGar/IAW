1a0943e6f9f0a19378bb69382ecfb046d9566327
JoseLuisGarGar <joseluisgarciagarrachon@gmail.com>
2020-11-17 20:33:27 +0100

    ff

--- practicas/p-iaw-3/ejercicio1.php
+++ practicas/p-iaw-3/ejercicio1.php
@@ -15,12 +15,17 @@
                 $errores['nombre'] = "No se ha introducido nombre a la fiesta.";
               }
             
-            if(isset($_POST['personas']) == ""){
+            if(isset($_POST['personas']) && ($_POST['personas'] == "")){
                 $errores['personas'] = "No se ha introducido el nº de personas.";
             }
+            if(isset($_POST['tipo']) && ($_POST['tipo'] == "")){
+                $errores['tipo'] = "No se ha introducido el tipo de música.";
+            }
+            if((!isset($_POST['espacio'])) || (isset($_POST['espacio']) && (count($_POST['espacio']) < 2))){
+                $errores['espacio'] = "No se han introducido 2 espacios o mas.";
+            }
         }
-        if (count($errores) == 0 && (isset($_POST['enviar']))){
-            echo "Formulario: ".$_POST['formulario']. " <br>";
+        if (count($errores) == 0 && (isset($_POST['Enviar']))){
             echo "El nombre de la fiesta es: ".$_POST['nombre']. " <br>";
             echo "Acudirán ".$_POST['personas']. " personas </br>";
             echo "La fiesta será: ".$_POST['pública']. " <br>";
@@ -47,20 +52,28 @@
             echo $errores['nombre'];
             }?>
             <br>
-            Número de personas <input type='number' name='personas' min ='0'><br>
-            Tipo de fiesta
-            <input type='radio' name='pública' value='pública' checked> Pública
-            <input type='radio'  name='privada' value='privada'> Privada<br>
+            Número de personas <input type='number' name='personas' min ='1'>
+            <?php 
+                if(isset($errores['personas'])){
+                echo $errores['personas'];
+            }?>
+            <br>Tipo de fiesta
+            <input type='radio' name='fiesta' value='pública' checked> Pública
+            <input type='radio'  name='fiesta' value='privada'> Privada<br>
             Tipo de música  
             <select name='tipo'>
+            <option value=''>Introduce un tipo de música</option>
             <option value='pop'>Pop</option>
             <option value='rock'>Rock</option>
             <option value='jazz'>Jazz</option>
             <option value='indie'>Indie</option>       
-            </select><br>
-            <?php if(isset($errores['tipo'])){
+            </select>
+            
+            <?php 
+            if(isset($errores['tipo'])){
             echo $errores['tipo'];
             }?>
+            <br>
             Espacios para la fiesta: <br><input type='checkbox' name='espacio[]' value='barras' min="2"> Barras<br>
             <input type='checkbox'  name='espacio[]' value='escenarios' checked> Escenarios<br>
             <input type='checkbox'  name='espacio[]' value='jardin'> Jardín<br>
@@ -69,10 +82,11 @@
             <?php if(isset($errores['espacio'])){
             echo $errores['espacio'];
             }?>
+            <br>
             Comentario de interés sobre a fiesta <br>
             <textarea name='comentarios' cols='40' rows='3'>
             </textarea><br>
-            <input type='submit' name='enviar' value='Enviar' >
+            <input type='submit' name='Enviar' value='Enviar' >
             </form>
         <?php    
         }
