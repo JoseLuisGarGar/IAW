76b968309eaf9ba5edda2fcab91c1fc3b57b5093
JoseLuisGarGar <joseluisgarciagarrachon@gmail.com>
2020-12-01 20:36:56 +0100

    t

--- practicas/p-iaw-7/calculadora.php
+++ practicas/p-iaw-7/calculadora.php
@@ -11,7 +11,7 @@
         
         <?php
         echo "<form action='calculadora.php' method='POST' enctype='multipart/form-data'>";
-        echo "Introduce 2 números para sumarlos: <br>";
+        echo "Introduce 2 números para sumarlos/restarlos/multiplicarlos/dividirlos: <br>";
         echo "Número 1: <input type='number' name='input_a'><br>";
         echo "Número 2: <input type='number' name='input_b'><br>";
         echo "<input type='submit' value='Sumar' name='sumar'>";
